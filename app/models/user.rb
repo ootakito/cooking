dad4ed978f70f3257b0a7b23fbf69a58bb61fa23
ootakito <165157559+ootakito@users.noreply.github.com>
2024-05-03 13:57:13 +0900

    新規登録画面が表示できた

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
+
+  #has_many :recipes
+  #has_many :likes
 end
